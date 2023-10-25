683f613dc24395867ba8f3e57fc955c0a0ef9042
WebDevMullins <webdevmullins@gmail.com>
2023-10-25 13:56:52 -0500

    add connection - db.js

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import mysql from 'mysql2'
+
+dotenv.config()
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: 'company_db'
+})
+
+db.connect(function (err) {
+	err ? console.error('error connecting: ' + err.stack) : console.log('connected as id ' + db.threadId)
+})
+
+export { db }
