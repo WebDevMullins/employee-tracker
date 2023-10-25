8284a8b0703b0bd5748d0cf0b9f4b2bed9fe4cd9
WebDevMullins <webdevmullins@gmail.com>
2023-10-25 18:02:06 -0500

    add console.table, format output off getAllDepartments

--- models/department.js
+++ models/department.js
@@ -1,14 +1,18 @@
+import * as cTable from 'console.table'
 import { mainMenu } from '../cli/mainMenu.js'
 import { db } from '../config/db.js'
 
 function getAllDepartments() {
-	const sql = 'SELECT id AS id, name AS department FROM department'
+	const sql = 'SELECT id, name FROM department'
 	db.promise()
 		.query(sql)
 		.then(([departments]) => {
-			// console.log(`================================================`)
+			console.log('')
+			console.log(`================================================`)
+			console.log('')
 			console.table(departments)
-			// console.log(`================================================`)
+			console.log(`================================================`)
+			console.log('')
 			mainMenu()
 		})
 		.catch((err) => console.error(err))
