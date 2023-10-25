8c6f5691d4288e130b83c7aa6baa162c2c3552a7
WebDevMullins <webdevmullins@gmail.com>
2023-10-25 17:48:14 -0500

    update to call main menu after selection

--- cli/mainMenu.js
+++ cli/mainMenu.js
@@ -1,28 +1,31 @@
 import inquirer from 'inquirer'
 import { getAllDepartments } from '../models/department.js'
 
-async function mainMenu() {
-	const { selected } = await inquirer.prompt({
-		type: 'list',
-		name: 'selected',
-		message: 'What would you like to do?',
-		choices: [
-			'View All Departments',
-			'View All Roles',
-			'View All Employees',
-			'Add a Department',
-			'Add a Role',
-			'Add an Employee',
-			'Update an Employee Role',
-			'Quit'
-		],
-		loop: false
-	})
-	switch (selected) {
-		case 'View All Departments':
-			getAllDepartments()
-			break
-	}
+const mainMenu = () => {
+	inquirer
+		.prompt({
+			type: 'list',
+			name: 'selected',
+			message: 'What would you like to do?',
+			choices: [
+				'View All Departments',
+				'View All Roles',
+				'View All Employees',
+				'Add a Department',
+				'Add a Role',
+				'Add an Employee',
+				'Update an Employee Role',
+				'Quit'
+			],
+			loop: false
+		})
+		.then(({selected}) => {
+			switch (selected) {
+				case 'View All Departments':
+					getAllDepartments()
+					break
+			}
+		})
 }
 
 export { mainMenu }
--- models/department.js
+++ models/department.js
@@ -1,14 +1,17 @@
+import { mainMenu } from '../cli/mainMenu.js'
 import { db } from '../config/db.js'
 
 function getAllDepartments() {
-	const sql = 'SELECT id, name FROM department'
+	const sql = 'SELECT id AS id, name AS department FROM department'
 	db.promise()
 		.query(sql)
 		.then(([departments]) => {
+			// console.log(`================================================`)
 			console.table(departments)
+			// console.log(`================================================`)
+			mainMenu()
 		})
-		.catch(console.error)
-		.then(() => db.end())
+		.catch((err) => console.error(err))
 }
 
 export { getAllDepartments }
